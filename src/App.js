84460e484ba27eedd65b73de4180cdff6ece660f
aurelio <aurelioarcabascio@gmail.com>
2020-10-17 01:38:13 -0400

    fixed a bug

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 		<div className="app">
 			<button className="random-button" onClick={() => setVideoIndex(Math.floor(Math.random() * (videos.length - 1)))}>New Video</button>
 			<div className="video-player">
-				<iframe width={width} height={height} src={videos[videoIndex]}></iframe>
+				<iframe title="video-player" width={width} height={height} src={videos[videoIndex]}></iframe>
 			</div>
 		</div>
   );
