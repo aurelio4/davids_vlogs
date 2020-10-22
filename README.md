ba30cd050369e8efc80c5ea42179cf39d94d593e
aurelio <aurelioarcabascio@gmail.com>
2020-10-22 03:24:17 -0400

    new layout, just gotta work out the math

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## To add:
  - specific vlog number
+ - make video player responsive size (not that anyone's trying to break our site, but.. never know!)
  - add backend to check for latest videos
--- src/App.js
+++ src/App.js
@@ -6,10 +6,22 @@ function App() {
 	const width = window.innerWidth < 200 ? 200 : window.innerWidth / 1.2
 	const height = window.innerHeight < 200 ? 200 : window.innerHeight / 1.2
 	const [videoIndex, setVideoIndex] = useState(Math.floor(Math.random() * (videos.length - 1)))
+	const [vlogNumber, setVlogNumber] = useState(Math.floor(Math.random() * (videos.length - 1)))
+
+	const checkVideo = () => {
+		if(vlogNumber > videos.length) {
+			alert('Not a valid vlog!')
+		} else {
+			setVideoIndex(vlogNumber)
+		}
+	}
 
   return (
 		<div className="app">
-			<button className="random-button" onClick={() => setVideoIndex(Math.floor(Math.random() * (videos.length - 1)))}>New Video</button>
+			<div className="nav">
+				<input type="number" className="vlog-number-input" name="vlog-number-input" value={vlogNumber} onChange={e => setVlogNumber(e.target.value)} />
+				<button className="vlog-number-button" onClick={() => checkVideo()}>New Vlog</button>
+			</div>
 			<div className="video-player">
 				<iframe title="video-player" width={width} height={height} src={`https://www.youtube.com/embed/${videos[videoIndex]}`}></iframe>
 			</div>
